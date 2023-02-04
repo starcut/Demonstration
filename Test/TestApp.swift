f6f9ae66adc219558a9abfb735154e181f273b9c
shusuke shimizu <stella.scraps@gmail.com>
2023-02-05 01:40:29 +0900

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by 清水脩輔 on 2023/02/05.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
