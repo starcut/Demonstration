fdd49d686a8e94a806f666bdb69a8e2555ab8049
shusuke shimizu <stella.scraps@gmail.com>
2023-03-27 15:53:28 +0900

    列挙体ループを追加

---
+++ Test/Const/EnumAttribute.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+enum MagicAttribute: String, CaseIterable {
+    case fire = "メラ"
+    case ice = "ヒャド"
+    case wind = "バギ"
+    case electro = "デイン"
+    case light = "イオ"
+    case dark = "ドルマ"
+    case earth = "ジバリア"
+    case death = "ザキ"
+}
