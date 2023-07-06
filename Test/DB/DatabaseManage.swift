85483e0c18f4b0d154fe490ca285d1e2a1fd4d98
shusuke shimizu <stella.scraps@gmail.com>
2023-07-07 01:40:45 +0900

    データベースのソート

--- Test/DB/DatabaseManage.swift
+++ Test/DB/DatabaseManage.swift
@@ -1,9 +1,25 @@
 import Foundation
 import RealmSwift
 
+enum DataBaseOrder: String, CaseIterable, Identifiable {
+    case idAscendingOrder = "ID昇順"
+    case idDescendingOrder = "ID降順"
+    case titleAscendingOrder = "タイトル昇順"
+    case titleDescendingOrder = "タイトル降順"
+    case registerDateAscendingOrder = "登録が古い順"
+    case registerDateDescendingOrder = "登録が新しい順"
+    case updateDateAscendingOrder = "更新が古い順"
+    case updateDateDescendingOrder = "更新が新しい順"
+    
+    var id: String { rawValue }
+}
+
+
 class DatabaseManager {
     static let shared = DatabaseManager()
     var realm: Realm? = nil
+    
+    @Published var selectedOrder: DataBaseOrder = .idAscendingOrder
     
     private init() {
         
@@ -13,9 +29,10 @@ class DatabaseManager {
         do {
             self.realm = try? Realm()
         }
-        guard let objects = self.realm?.objects(DatabaseTableData.self) else {
+        guard var objects = self.realm?.objects(DatabaseTableData.self) else {
             return []
         }
+        objects = sortedObjects(objects: objects)
         return Array(objects)
     }
     
@@ -44,6 +61,51 @@ class DatabaseManager {
             try? self.realm?.write {
                 self.realm?.delete(data)
             }
+        }
+    }
+    
+    private func sortedObjects(objects: Results<DatabaseTableData>) -> Results<DatabaseTableData> {
+        switch DatabaseManager.shared.selectedOrder {
+        case .idAscendingOrder:
+            let sortDescriptors = [
+                SortDescriptor(keyPath: "id", ascending: true),
+            ]
+            return objects.sorted(by: sortDescriptors)
+        case .idDescendingOrder:
+            let sortDescriptors = [
+                SortDescriptor(keyPath: "id", ascending: false),
+            ]
+            return objects.sorted(by: sortDescriptors)
+        case .titleAscendingOrder:
+            let sortDescriptors = [
+                SortDescriptor(keyPath: "title", ascending: true),
+            ]
+            return objects.sorted(by: sortDescriptors)
+        case .titleDescendingOrder:
+            let sortDescriptors = [
+                SortDescriptor(keyPath: "title", ascending: false),
+            ]
+            return objects.sorted(by: sortDescriptors)
+        case .registerDateAscendingOrder:
+            let sortDescriptors = [
+                SortDescriptor(keyPath: "registerDate", ascending: true),
+            ]
+            return objects.sorted(by: sortDescriptors)
+        case .registerDateDescendingOrder:
+            let sortDescriptors = [
+                SortDescriptor(keyPath: "registerDate", ascending: false),
+            ]
+            return objects.sorted(by: sortDescriptors)
+        case .updateDateAscendingOrder:
+            let sortDescriptors = [
+                SortDescriptor(keyPath: "updateDate", ascending: true),
+            ]
+            return objects.sorted(by: sortDescriptors)
+        case .updateDateDescendingOrder:
+            let sortDescriptors = [
+                SortDescriptor(keyPath: "updateDate", ascending: false),
+            ]
+            return objects.sorted(by: sortDescriptors)
         }
     }
 }
